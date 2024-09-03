211b1a8c8c66099d65351bf642f2fff50ec863b9
desalutar <desalutare@gmail.com>
2024-09-03 14:51:01 +0300

    first commit

---
+++ Todos_EffectiveMobile/Todos_EffectiveMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todos_EffectiveMobileApp.swift
+//  Todos_EffectiveMobile
+//
+//  Created by Ишхан Багратуни on 03.09.24.
+//
+
+import SwiftUI
+
+@main
+struct Todos_EffectiveMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
